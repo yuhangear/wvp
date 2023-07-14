29a29057622ae7281cb1e2228fef619082593ac4
yuhangear <yuhangear@163.com>
2023-07-14 20:36:20 +0800

    add start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 service mysql restart
 service redis-server restart
 
 cd /root/ZLMediaKit/release/linux/Debug 
-nohup ./MediaServer -d  &
+nohup ./MediaServer  > log1 2 1>&1 &
 cd /root/wvp-GB28181-pro/target
-nohup java -jar wvp-pro-2.6.9-071316*.jar &
+nohup java -jar wvp-pro*.jar > log2 2 1>&1 &
+
